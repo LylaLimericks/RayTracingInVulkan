309b9bc8d18db893ce0472894ecf9b196f1c0c87
LylaT <lylatelkamp@gmail.com>
2025-11-05 18:51:31 -0800

    Implementing the collision detection for a sphere

--- src/InOneWeekend/main.cpp
+++ src/InOneWeekend/main.cpp
@@ -4,7 +4,20 @@
 
 #include <iostream>
 
+bool hitSphere(const point3& center, double radius, const ray& r) {
+    vec3 oc = center - r.origin();
+    auto a = dot(r.direction(), r.direction());
+    auto b = -2.0 * dot(r.direction(), oc);
+    auto c = dot(oc, oc) - radius*radius;
+    auto discriminant = b * b - 4 * a * c;
+    return (discriminant >= 0);
+}
+
 color rayColor(const ray& r) {
+    if (hitSphere(point3(0, 0, -1), 0.5, r)) {
+        return color(1, 0, 0);
+    }
+    
     vec3 unitDirection = unitVector(r.direction());
     auto a = 0.5*(unitDirection.y() + 1.0);
     return (1.0-a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
@@ -49,6 +62,6 @@ int main() {
         }
     }
 
-    std::clog << "\rDone.           \n";
+    std::clog << "\rDone.                   \n";
     
 }
