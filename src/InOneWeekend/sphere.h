899fbf5c67117d73bcee6cc35df765ea63c43157
LylaT <lylatelkamp@gmail.com>
2025-11-05 19:46:28 -0800

    Adding sphere.h

---
+++ src/InOneWeekend/sphere.h
@@ -0,0 +1,46 @@
+#ifndef SPHERE_H
+#define SPHERE_H
+
+#include "hittable.h"
+#include "vec3.h"
+
+class sphere : public hittable {
+    public:
+        sphere(const point3& center, double radius): center(center), radius(std::fmax(0, radius)) {}
+
+        bool hit(const ray& r, double tMin, double tMax, hitRecord& rec) const override {
+            vec3 oc = center - r.origin();
+            auto a = r.direction().lengthSquared();
+            auto h = dot(r.direction(), oc);
+            auto c = oc.lengthSquared() - radius*radius;
+
+            auto discriminant = h*h - a*c;
+            if (discriminant < 0) {
+                return false;
+            }
+
+            auto sqrtd = std::sqrt(discriminant);
+
+            // We want to find the first point of contact
+            auto root = (h - sqrtd) / a;
+            if (root <= tMin || tMax <= root) {
+                root = (h + sqrtd) / a;
+                if (root <= tMin || tMax <= root) {
+                    return false;
+                }
+            }
+
+            rec.t = root;
+            rec.p = r.at(rec.t);
+            rec.normal = (rec.p - center) / radius;
+
+            return true;
+        }
+
+
+    private:
+        point3 center;
+        double radius;
+};
+
+#endif
