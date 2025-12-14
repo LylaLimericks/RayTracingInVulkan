e7dd38102d39a5e30aeb320eaddb526437289660
LylaT <lylatelkamp@gmail.com>
2025-12-14 13:51:19 -0800

    Adding a quick Window Manager header
    Used to provide an abstraction layer

---
+++ src/WindowManager.hpp
@@ -0,0 +1,11 @@
+#ifndef WINDOW_MANAGER_H
+#define WINDOW_MANAGER_H
+
+#include <cstdint>
+
+class WindowManager {
+public:
+  const char **getRequiredExtensions(uint32_t *extensionCount);
+};
+
+#endif
