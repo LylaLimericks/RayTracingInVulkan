7dca6fe9f0fb714aa093130f940e36ad416d2b95
LylaT <lylatelkamp@gmail.com>
2025-12-27 20:24:16 -0800

    Creating SwapChain Vulkan class

---
+++ src/Vulkan/SwapChain.cpp
@@ -0,0 +1,87 @@
+#include "SwapChain.hpp"
+#include "Vulkan/Device.hpp"
+#include "Vulkan/Surface.hpp"
+#include "Vulkan/Window.hpp"
+#include "vulkan/vulkan.hpp"
+#include <memory>
+#include <vulkan/vulkan_core.h>
+#include <vulkan/vulkan_raii.hpp>
+
+namespace Vulkan {
+
+SwapChain::SwapChain(const Device &device, vk::PresentModeKHR presentMode) {
+  const auto physicalDevice = device.PhysicalDevice();
+  const Surface &surface = device.Surface();
+  const Window &window = surface.Window();
+  const auto surfaceCapabilities = physicalDevice.getSurfaceCapabilitiesKHR(*surface.Handle());
+
+  surfaceFormat = chooseSwapSurfaceFormat(physicalDevice.getSurfaceFormatsKHR());
+  extent = chooseSwapExtent(window, surfaceCapabilities);
+
+  auto minImageCount = std::max(MAX_IMAGE_COUNT, surfaceCapabilities.minImageCount);
+  minImageCount =
+      (surfaceCapabilities.maxImageCount > 0 && minImageCount > surfaceCapabilities.maxImageCount)
+          ? surfaceCapabilities.maxImageCount
+          : minImageCount;
+
+  vk::SwapchainCreateInfoKHR swapChainCreateInfo{
+      .flags = vk::SwapchainCreateFlagsKHR(),
+      .surface = *surface.Handle(),
+      .minImageCount = minImageCount,
+      .imageFormat = surfaceFormat.format,
+      .imageColorSpace = surfaceFormat.colorSpace,
+      .imageExtent = extent,
+      .imageArrayLayers = 1,
+      .imageUsage = vk::ImageUsageFlagBits::eColorAttachment,
+      .imageSharingMode = vk::SharingMode::eExclusive,
+      .preTransform = surfaceCapabilities.currentTransform,
+      .compositeAlpha = vk::CompositeAlphaFlagBitsKHR::eOpaque,
+      .presentMode = choosePresentMode(physicalDevice.getSurfacePresentModesKHR(*surface.Handle()), presentMode),
+      .clipped = true,
+      .oldSwapchain = VK_NULL_HANDLE,
+  };
+
+  if (device.GraphicsFamilyIndex() != device.PresentFamilyIndex()) { // If we need to have separate graphics and present families
+    uint32_t queueFamilyIndices[] = {device.GraphicsFamilyIndex(), device.PresentFamilyIndex()};
+    swapChainCreateInfo.imageSharingMode = vk::SharingMode::eConcurrent;
+    swapChainCreateInfo.queueFamilyIndexCount = 2;
+    swapChainCreateInfo.pQueueFamilyIndices = queueFamilyIndices;
+  }
+
+  swapChain = std::make_unique<vk::raii::SwapchainKHR>(*device.Handle(), swapChainCreateInfo);
+  swapChainImages = swapChain->getImages();
+}
+
+vk::SurfaceFormatKHR SwapChain::chooseSwapSurfaceFormat(const std::vector<vk::SurfaceFormatKHR> &availableFormats) const {
+
+  for (const auto &availableFormat : availableFormats) {
+    if (availableFormat.format == vk::Format::eB8G8R8A8Srgb && availableFormat.colorSpace == vk::ColorSpaceKHR::eSrgbNonlinear) {
+      return availableFormat;
+    }
+  }
+
+  return availableFormats[0];
+}
+
+vk::PresentModeKHR SwapChain::choosePresentMode(const std::vector<vk::PresentModeKHR> &presentModes, vk::PresentModeKHR presentMode) {
+  if (std::find(presentModes.begin(), presentModes.end(), presentMode) != presentModes.end()) {
+    return presentMode;
+  }
+
+  return vk::PresentModeKHR::eFifo;
+}
+
+vk::Extent2D SwapChain::chooseSwapExtent(const Window &window, const vk::SurfaceCapabilitiesKHR &capabilities) {
+  if (capabilities.currentExtent.width != std::numeric_limits<uint32_t>::max()) {
+    return capabilities.currentExtent;
+  }
+
+  const auto windowSize = window.FramebufferSize();
+
+  return {
+      std::clamp<uint32_t>(windowSize.width, capabilities.minImageExtent.width, capabilities.maxImageExtent.width),
+      std::clamp<uint32_t>(windowSize.height, capabilities.minImageExtent.height, capabilities.maxImageExtent.height),
+  };
+}
+
+} // namespace Vulkan
---
+++ src/Vulkan/SwapChain.hpp
@@ -0,0 +1,31 @@
+#ifndef SWAP_CHAIN_H
+#define SWAP_CHAIN_H
+
+#include "vulkan/vulkan.hpp"
+#include <vulkan/vulkan_raii.hpp>
+namespace Vulkan {
+
+class Device;
+class Window;
+
+class SwapChain {
+
+public:
+  SwapChain(const Device &device, vk::PresentModeKHR presentMode);
+
+  const uint32_t MAX_IMAGE_COUNT = 3u;
+
+private:
+  std::unique_ptr<vk::raii::SwapchainKHR> swapChain;
+  std::vector<vk::Image> swapChainImages;
+
+  vk::SurfaceFormatKHR surfaceFormat;
+  vk::Extent2D extent;
+
+  vk::SurfaceFormatKHR chooseSwapSurfaceFormat(const std::vector<vk::SurfaceFormatKHR> &formats) const;
+  vk::PresentModeKHR choosePresentMode(const std::vector<vk::PresentModeKHR> &presentModes, const vk::PresentModeKHR presentMode);
+  vk::Extent2D chooseSwapExtent(const Window &window, const vk::SurfaceCapabilitiesKHR &capabilities);
+};
+} // namespace Vulkan
+
+#endif
