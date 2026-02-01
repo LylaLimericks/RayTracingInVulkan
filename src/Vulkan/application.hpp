14b976f423ec03e4353aebd22b120d9d17a6e796
LylaT <lylatelkamp@gmail.com>
2026-02-01 15:48:35 -0800

    Adding initial vulkan application and implementing up to gpipe

--- src/Vulkan/application.hpp
+++ src/Vulkan/application.hpp
@@ -1,7 +1,147 @@
 #pragma once
 
+#include "vulkan/vulkan.hpp"
+#include <GLFW/glfw3.h>
+#include <fstream>
+#include <iostream>
+#include <vulkan/vulkan_raii.hpp>
+#include <vulkan/vulkan_to_string.hpp>
 namespace Vulkan {
 
-class Application {};
+class Application {
+public:
+  // Application();
+  void mainLoop();
+
+private:
+  GLFWwindow *window = nullptr;
+  vk::raii::Context context;
+  vk::raii::Instance instance;
+  vk::raii::DebugUtilsMessengerEXT debugMessenger = nullptr;
+  vk::raii::SurfaceKHR surface = nullptr;
+  vk::raii::PhysicalDevice physicalDevice = nullptr;
+  vk::raii::Device device = nullptr;
+  uint32_t queueIndex = ~0;
+  vk::raii::Queue queue = nullptr;
+  vk::raii::SwapchainKHR swapChain = nullptr;
+  std::vector<vk::Image> swapChainImages;
+  vk::SurfaceFormatKHR swapChainSurfaceFormat;
+  vk::Extent2D swapChainExtent;
+  std::vector<vk::raii::ImageView> swapChainImageViews;
+
+  vk::raii::PipelineLayout pipelineLayout = nullptr;
+  vk::raii::Pipeline graphicsPipeline = nullptr;
+
+  vk::raii::CommandPool commandPool = nullptr;
+  std::vector<vk::raii::CommandBuffer> commandBuffers;
+
+  std::vector<vk::raii::Semaphore> presentCompleteSemaphote;
+  std::vector<vk::raii::Semaphore> renderFinishedSemaphore;
+  std::vector<vk::raii::Fence> inFlightFences;
+  uint32_t semaphoreIndex = 0;
+  uint32_t currentFrame = 0;
+  std::vector<const char *> requiredDeviceExtension = {
+      vk::KHRSwapchainExtensionName,
+      vk::KHRSpirv14ExtensionName,
+      vk::KHRSynchronization2ExtensionName,
+      vk::KHRCreateRenderpass2ExtensionName,
+  };
+
+  void initWindow();
+  void initVulkan();
+
+  void createInstance();
+  std::vector<const char *> getRequiredExtensions();
+  void setupDebugMessenger();
+  void createSurface();
+
+  void pickPhysicalDevice();
+  void createLogicalDevice();
+
+  void createSwapChain();
+  vk::Extent2D chooseSwapExtent(const vk::SurfaceCapabilitiesKHR &capabilities);
+  void createImageViews();
+  void createGraphicsPipeline();
+
+  void createCommandPool();
+  void createCommandBuffers();
+  void createSyncObjects();
+
+  void recordCommandBuffer(uint32_t imageIndex);
+  void drawFrame();
+
+  [[nodiscard]] vk::raii::ShaderModule
+  createShaderModule(const std::vector<char> &code);
+
+  void transition_image_layout(uint32_t imageIndex, vk::ImageLayout old_layout,
+                               vk::ImageLayout new_layout,
+                               vk::AccessFlags2 src_access_mask,
+                               vk::AccessFlags2 dst_access_mask,
+                               vk::PipelineStageFlags2 src_stage_mask,
+                               vk::PipelineStageFlags2 dst_stage_mask);
+
+  void cleanUp();
+
+  static const uint32_t chooseSwapMinImageCount(
+      const vk::SurfaceCapabilitiesKHR &surfaceCapabilities) {
+    auto minImageCount = std::max(3u, surfaceCapabilities.minImageCount);
+    if ((0 < surfaceCapabilities.maxImageCount) &&
+        (surfaceCapabilities.maxImageCount < minImageCount)) {
+      minImageCount = surfaceCapabilities.maxImageCount;
+    }
+
+    return minImageCount;
+  }
+
+  static const vk::SurfaceFormatKHR chooseSwapSurfaceFormat(
+      const std::vector<vk::SurfaceFormatKHR> &availableFormats) {
+    assert(!availableFormats.empty());
+    const auto formatIt =
+        std::ranges::find_if(availableFormats, [](const auto &format) {
+          return format.format == vk::Format::eB8G8R8A8Srgb &&
+                 format.colorSpace == vk::ColorSpaceKHR::eSrgbNonlinear;
+        });
+
+    return formatIt != availableFormats.end() ? *formatIt : availableFormats[0];
+  }
+
+  static const vk::PresentModeKHR chooseSwapPresentMode(
+      const std::vector<vk::PresentModeKHR> &availablePresentModes) {
+    assert(std::ranges::any_of(availablePresentModes, [](auto presentMode) {
+      return presentMode == vk::PresentModeKHR::eFifo;
+    }));
+    return std::ranges::any_of(availablePresentModes,
+                               [](const vk::PresentModeKHR value) {
+                                 return vk::PresentModeKHR::eMailbox == value;
+                               })
+               ? vk::PresentModeKHR::eMailbox
+               : vk::PresentModeKHR::eFifo;
+  }
+
+  static VKAPI_ATTR vk::Bool32 VKAPI_CALL debugCallback(
+      vk::DebugUtilsMessageSeverityFlagBitsEXT severity,
+      vk::DebugUtilsMessageTypeFlagsEXT type,
+      const vk::DebugUtilsMessengerCallbackDataEXT *pCallbackData, void *) {
+    if (severity == vk::DebugUtilsMessageSeverityFlagBitsEXT::eError ||
+        severity == vk::DebugUtilsMessageSeverityFlagBitsEXT::eWarning) {
+      std::cerr << "validation layer: type " << vk::to_string(type)
+                << " msg: " << pCallbackData->pMessage << std::endl;
+    }
+
+    return vk::False;
+  }
+
+  static const std::vector<char> readFile(const std::string &filename) {
+    std::ifstream file(filename, std::ios::ate | std::ios::binary);
+    if (!file.is_open()) {
+      throw std::runtime_error("failed to open file!");
+    }
+    std::vector<char> buffer(file.tellg());
+    file.seekg(0, std::ios::beg);
+    file.read(buffer.data(), static_cast<std::streamsize>(buffer.size()));
+    file.close();
+    return buffer;
+  }
+};
 
 } // namespace Vulkan
